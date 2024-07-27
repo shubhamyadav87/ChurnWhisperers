e46a4604937f0b5b56c6cf2672b112e1f68f0741
Shubham Yadav <101976389+shubhamyadav87@users.noreply.github.com>
2024-07-27 11:30:45 +0530

    Update model.py

--- model.py
+++ model.py
@@ -3,45 +3,51 @@ import pickle
 import streamlit as st
 
 #loading_save_model
-loaded_model=pickle.load(open("Diabetes_trained_model.sav",'rb'))
+loaded_model=pickle.load(open("Customer_churn_prediction.sav",'rb'))
 
 #creating_a_function
-def Diabetes_prediction(input_data):
-    #changing input_data into array
-    input_data_as_array=np.asarray(input_data)
-    #reshaping the array
-    input_data_reshaped=input_data_as_array.reshape(1,-1)
-    #prediction
-    prediction=loaded_model.predict(input_data_reshaped)
-    print(prediction)
-    #condition
-    if(prediction[0]==0):
-      return "The person is not diabetic"
-    else:
-      return "The person is diabetic"
-  
+def Customer_churn_prediction(input_data):
+  #changing input_data into array
+  input_data_as_array=np.asarray(input_data)
+  #reshaping the array
+  input_data_reshaped=input_data_as_array.reshape(1,-1)
+  #prediction
+  prediction=loaded_model.predict(input_data_reshaped)
+  print(prediction)
+  #condition
+  if(prediction[0]==0):
+    print("The person will not exit")
+  else:
+    print("The person will exit")
+    
 
 def main():
     
     #title
-    st.title("Diabetes_Prediction")
+    st.title("ChurnWhisperers")
     
     #input_data    
-    Pregnancies=st.text_input("Enter No. of pregnancies")
-    Glucose=st.text_input("Enter Glucose level")
-    BloodPressure=st.text_input("Enter your Blood pressure(BP)")
-    BMI=st.text_input("Enter your BMI(Body-mass-index)")
-    Age=st.text_input("Enter your Age")
+    CreditScore=st.text_input("Enter CreditScore")
+    Age=st.text_input("Enter Your Age")
+    Tenure=st.text_input("Enter your Tenure")
+    Balance=st.text_input("Enter your Balance")
+    NumOfProducts=st.text_input("Enter Num Of Products you owned")
+    HasCrCard=st.text_input("Do you have Credit card")
+    IsActiveMember=st.text_input("you are a active member")
+    EstimatedSalary=st.text_input("Enter Estimated Salary")
+    Geography_Germany=st.text_input("you live in Germany")
+    Geography_Spain=st.text_input("you live in spain")
+    Gender_Male=st.text_input("You are a male")
     
     #code_for_prediction
-    Diagnosis=""
+    predict=""
     
     #create_button for results
     
     if st.button("Results:"):
-        Diagnosis=Diabetes_prediction([Pregnancies,Glucose,BloodPressure,BMI,Age])
+        predict=Customer_churn_prediction([CreditScore,Age,Tenure,Balance,NumOfProducts,HasCrCard,IsActiveMember,EstimatedSalary,Geography_Germany,Geography_Spain,Gender_Male])
     
-    st.success(Diagnosis)
+    st.success(predict)
     
     
 if __name__=='__main__':
